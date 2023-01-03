344ae2ccf08f6d689a865d7d7b9e1b00972d994b
Natan Fernandes <113848526+natanfds@users.noreply.github.com>
2023-01-03 13:23:52 -0300

    [F] Register start and end time of process [D] Improve reading

--- res_watcher/__init__.py
+++ res_watcher/__init__.py
@@ -1,19 +1,22 @@
 from time import sleep
-from logging import INFO as level_info
-from os import getpid
-from psutil import Process
 from threading import Thread
+from logging import INFO as level_info
 from csv_logger import CsvLogger
+from psutil import Process
+from os import getpid
+
+
 
 
 class ResWatcher(object):
   
-
     def __init__(self,
                  delta:int=2, max_size:int=1024, max_files:int=4,
-                 delimiter:str=';',file_path:str='consumption.csv'
+                 delimiter:str=';',file_path:str='consumption.csv',
+                 custom_levels=['PROCESS']
                 ):
 
+        self.__file_path = file_path
         self.__delimiter=delimiter
         self.__delta=delta
         self.__loop = True
@@ -22,22 +25,28 @@ class ResWatcher(object):
        
         self.log_options={
            'filename': file_path,
-           'delimiter': self.__delimiter,
+           'delimiter': delimiter,
            'level': level_info,
-           'fmt': f'%(asctime)s{self.__delimiter}%(levelname)s{self.__delimiter}%(message)s',
+           'fmt': f'%(asctime)s{delimiter}%(levelname)s{delimiter}%(message)s',
            'datefmt': '%Y/%m/%d %H:%M:%S',
            'max_size': max_size,
            'max_files': max_files,
-           'header': ['DATE_TIME', 'LEVEL', 'RAM', 'CPU'] 
+           'header': ['DATE_TIME', 'LEVEL', 'RAM', 'CPU'],
+            'add_level_names': custom_levels
         }
         
-         
+        # Resource measurement
         self.__process = Process(getpid())
         self.__get_ram = lambda: round(self.__process.memory_info().rss / 1024 ** 2)
         self.__get_cpu = lambda: self.__process.cpu_percent()
         
+        # Custom Register
+        self.__register_process = lambda name, state: self.csvlogger.PROCESS([name, state])
+        self.process_start = lambda name:  self.__register_process(name, 'START')
+        self.process_finish = lambda name: self.__register_process(name, 'END')
         
-    def __register_ram_usage(self):  
+        
+    def __register_resources_usage(self):  
             while self.__loop:
                 self.csvlogger.info([
                     self.__get_ram(),
@@ -46,9 +55,10 @@ class ResWatcher(object):
                 sleep(self.__delta)
 
 
+        
     def start(self):
         self.csvlogger = CsvLogger(**self.log_options)
-        self.__th = Thread(target=self.__register_ram_usage)
+        self.__th = Thread(target=self.__register_resources_usage)
         self.__th.start()
 
     #TODO: gerar gráfico de consumo
